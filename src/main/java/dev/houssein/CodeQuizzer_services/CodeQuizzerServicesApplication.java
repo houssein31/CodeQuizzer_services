c64a526f170a846a64a6f1cc5d2647843c3acaba
Houssein Kharroubi <89645857+houssein31@users.noreply.github.com>
2025-03-14 12:41:40 -0400

    first commit

---
+++ src/main/java/dev/houssein/CodeQuizzer_services/CodeQuizzerServicesApplication.java
@@ -0,0 +1,20 @@
+package dev.houssein.CodeQuizzer_services;
+
+import dev.houssein.CodeQuizzer_services.role.Role;
+import dev.houssein.CodeQuizzer_services.role.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableJpaAuditing
+@EnableAsync
+public class CodeQuizzerServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeQuizzerServicesApplication.class, args);
+	}
+}
